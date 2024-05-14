edcfabd1fd732dbdc573749537d1a7190b26d744
alessandra3anoA <162600235+alessandra3anoA@users.noreply.github.com>
2024-05-14 12:08:41 -0300

    Add files via upload

--- ale-02/Aula Ling_lk/calculadora/main.py
+++ ale-02/Aula Ling_lk/calculadora/main.py
@@ -3,8 +3,9 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.lang import Builder
 from kivy.uix.button import Button
 from kivy.config import Config
-from kivy.graphics import Color, RoundedRectangle
-Config.set('graphics', 'width', '370') 
+from kivy.graphics import Color, Ellipse
+
+Config.set('graphics', 'width', '370')
 Config.set('graphics', 'height', '640')
 Builder.load_file('main.kv')
 
@@ -20,17 +21,32 @@ class Calculadora(BoxLayout):
             self.ids.display.text = str(eval(self.ids.display.text))
         except Exception as e:
             self.ids.display.text = 'Erro'
-class BotaoRedondo(Button):
+
+class MeuApp(BoxLayout):
     def __init__(self, **kwargs):
-        super(BotaoRedondo, self).__init__(**kwargs)
-        self.background_color = (0.2, 0.2, 0.2, 1)
-        self.font_size = 20
+        super(MeuApp, self).__init__(**kwargs)
+        self.ids.meu_botao.text = "Novo Texto"
+
+class BotaoCircular(Button):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
         self.size_hint = (None, None)
         self.size = (100, 100)
-        self.canvas.before.add(Color(rgba=self.background_color))
-        self.canvas.before.add(RoundedRectangle(size=self.size, pos=self.pos, radius=[20,]))
+        self.background_color = (0.4, 0.4, 0.4, 1)
+        self.background_normal = ''
+        self.background_down = ''
+        self.canvas.before.add(Color(0.2, 0.2, 0.2, 1))
+        self.canvas.before.add(Ellipse(pos=self.pos, size=self.size))
 
-Builder.load_string
+    def on_size(self, *args):
+        self.canvas.before.clear()
+        self.canvas.before.add(Color(0.2, 0.2, 0.2, 1))
+        self.canvas.before.add(Ellipse(pos=self.pos, size=self.size))
+class BotaoCustomizado(Button):
+    pass
+
+class MeuBotao(Button):
+    pass
 
 class AplicativoCalculadora(App):
     def build(self):
@@ -38,3 +54,5 @@ class AplicativoCalculadora(App):
 
 if __name__ == '__main__':
     AplicativoCalculadora().run()
+
+
